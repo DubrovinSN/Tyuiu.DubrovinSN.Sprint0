adbbaa9edce6d4e554723de37e54e62a7863397c
Tyuiu.DubrovinSN@outlook.com <stepan.09myromec@gmail.com>
2023-09-15 12:35:41 +0500

    Task2

---
+++ Tyuiu.DubrovinSN.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.DubrovinSN.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.DubrovinSN.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Степан";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Степан", res);
+        }
+    }
+}
