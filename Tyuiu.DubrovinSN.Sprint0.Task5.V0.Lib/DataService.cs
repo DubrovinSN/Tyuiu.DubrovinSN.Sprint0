834555ca92ba4c20bca7f73897704d0ffbd13672
Tyuiu.DubrovinSN@outlook.com <stepan.09myromec@gmail.com>
2023-09-15 17:38:41 +0500

    Task5

--- Tyuiu.DubrovinSN.Sprint0.Task5.V0.Lib/DataService.cs
+++ Tyuiu.DubrovinSN.Sprint0.Task5.V0.Lib/DataService.cs
@@ -8,5 +8,36 @@ namespace Tyuiu.DubrovinSN.Sprint0.Task5.V0.Lib
 {
     public class DataService
     {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Substracrion(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Переменную b = {0} на ноль делить нельзя", b);
+                return -1;
+            }
+            else
+            {
+                return a / b;
+            }
+
+
+        }
     }
 }
+
+
--- Tyuiu.DubrovinSN.Sprint0.Task5.V0.Test/DataServiceTest.cs
+++ Tyuiu.DubrovinSN.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using Tyuiu.DubrovinSN.Sprint0.Task5.V0.Lib;
 
 namespace Tyuiu.DubrovinSN.Sprint0.Task5.V0.Test
 {
@@ -7,8 +8,27 @@ namespace Tyuiu.DubrovinSN.Sprint0.Task5.V0.Test
     public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckedAdditionValid()
         {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
         }
+
+        [TestMethod]
+        public void CheckedSubstracrionValid()
+        {
+            Assert.AreEqual(5, DataService.Substracrion(10, 5));
+        }
+
+        [TestMethod]
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        [TestMethod]
+        public void CheckedDivision()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+
     }
 }
